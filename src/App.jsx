2d332d1387915b605cc34e3b1457cbf6217001c5
MandrykaR <mandrykaroman01@gmail.com>
2024-05-03 11:23:25 +0200

    Switching-Weeks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react'
 import Header from './components/header/Header.jsx'
 import Calendar from './components/calendar/Calendar.jsx'
-
 import { getWeekStartDate, generateWeekRange } from '../src/utils/dateUtils.js'
-
 import './common.scss'
 
 const App = () => {
 	const [weekStartDate, setWeekStartDate] = useState(new Date())
+  
 
 	const handlePrevWeek = () => {
 		setWeekStartDate(
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,12 +9,17 @@ const Header = ({ onPrevWeek, onNextWeek }) => {
 				<i className='fas fa-plus create-event-btn__icon'></i>Create
 			</button>
 			<div className='navigation'>
-				<button className='navigation__today-btn button'>Today</button>
+				<button
+					className='navigation__today-btn button'
+					onClick={onCurrentWeek}
+				>
+					Today
+				</button>
 				<button className='icon-button navigation__nav-icon'>
 					<i className='fas fa-chevron-left' onClick={onPrevWeek}></i>
 				</button>
 				<button className='icon-button navigation__nav-icon'>
-					<i className='fas fa-chevron-right' onCliFck={onNextWeek}></i>
+					<i className='fas fa-chevron-right' onClick={onNextWeek}></i>
 				</button>
 				<span className='navigation__displayed-month'></span>
 			</div>
