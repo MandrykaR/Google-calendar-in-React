7d8f203340e93030e898ce4318a231d3caa144fb
MandrykaR <mandrykaroman01@gmail.com>
2024-06-23 11:09:54 +0200

    h

--- src/components/event/Event.jsx
+++ src/components/event/Event.jsx
@@ -2,10 +2,7 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
-
-<<<<<<< HEAD
 import './event.scss';
-import { height } from '@fortawesome/free-solid-svg-icons/fa0';
 
 const Event = ({ id, height, marginTop, title, time, onDeleteEvent }) => {
   const [isButtonVisible, setButtonVisible] = useState(false);
@@ -49,51 +46,3 @@ Event.propTypes = {
   time: PropTypes.string.isRequired,
   onDeleteEvent: PropTypes.func.isRequired,
 };
-=======
-import './event.scss'
-
-const Event = ({ id, height, marginTop, title, time, onDeleteEvent }) => {
-   const [isButtonVisible, setButtonVisible] = useState(false)
-   const eventStyle = {
-      height,
-      marginTop,
-   }
-
-   const toggleButtonVisibility = e => {
-      e.stopPropagation()
-      setButtonVisible(!isButtonVisible)
-   }
-
-   return (
-      <div style={eventStyle} className='event' onClick={toggleButtonVisibility}>
-         <div className='event__content'>
-            <div className='event__title'>{title}</div>
-            <div className='event__time'>{time}</div>
-         </div>
-         {isButtonVisible && (
-            <button
-               className='delete-event-btn'
-               onClick={e => {
-                  e.stopPropagation()
-                  onDeleteEvent(id)
-               }}
-            >
-               <FontAwesomeIcon icon={faTrash} className='delete-event-btn__icon' />
-               Delete
-            </button>
-         )}
-      </div>
-   )
-}
-
-Event.propTypes = {
-   id: PropTypes.string.isRequired,
-   height: PropTypes.number.isRequired,
-   marginTop: PropTypes.number.isRequired,
-   title: PropTypes.string.isRequired,
-   time: PropTypes.string.isRequired,
-   onDeleteEvent: PropTypes.func.isRequired,
-}
->>>>>>> ad3ea296cd6a7a6be660667ffbd2abd9beb5d335
-
-export default Event;
